b6fdb862050758c41b373c567821bb1900149889
fabianoramos78 <contato@fabianoramos.dev>
2019-08-19 15:24:00 -0300

    Upload inicial

--- src/App.js
+++ src/App.js
@@ -1,26 +1,66 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
+import './styles/style.css'
+import MostraVoltas from './components/MostraVoltas'
+import MostraTempo from './components/MostraTempo'
+import MostraTempoTotal from './components/MostraTempoTotal'
+import Button from './components/Button'
+
 
 function App() {
+  const [numVoltas, setNumVoltas] = useState(0)  
+  const [running, setRunning] = useState(false)
+  const [tempo, setTempo] = useState(0)
+
+  useEffect(() => {
+    let timer = null
+    if(running){
+      timer = setInterval(() => {
+        setTempo(old => old + 1)
+      }, 1000)
+    }
+    return () => {
+      if(timer){
+        clearInterval(timer)
+      }
+    }    
+  }, [running])
+
+  const toggleRunning = () => {
+    setRunning(!running)
+  }
+
+  const increment = () => {
+    setNumVoltas(numVoltas + 1)
+  }
+
+  const decrement = () => {
+    if(numVoltas > 0){
+      setNumVoltas(numVoltas - 1)
+    }
+  }
+
+  const reset = () => {
+    setNumVoltas(0)
+    setTempo(0)
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <MostraVoltas voltas={numVoltas}/>
+      <Button text='-' onClick={decrement}/>
+      <Button text='+' onClick={increment}/><br/>
+      { 
+        numVoltas > 0 &&
+        <MostraTempoTotal tempo={Math.round(tempo)}/>
+      }
+      { 
+        numVoltas > 0 &&        
+        <MostraTempo tempo={Math.round(tempo/numVoltas)}/>
+      }
+      <Button text={running ? 'Pausar':'Iniciar'} onClick={toggleRunning}/>
+      <Button text='Reiniciar' onClick={reset}/>
     </div>
-  );
+  )
 }
 
 export default App;
